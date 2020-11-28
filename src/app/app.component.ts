40d864c92ba1d89de811369764ccbb51c5af2902
Voloshyna Jelena <codeismagicschool@gmail.com>
2020-11-28 20:04:07 +0200

    feat/add-connection-to-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  title = 'medeor';
+export class AppComponent implements OnInit {
+  code = 'DP1L1';
+  data: any;
+  constructor(private service: DataService) {}
+  ngOnInit() {
+    this.service.getData(this.code).subscribe((data) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
