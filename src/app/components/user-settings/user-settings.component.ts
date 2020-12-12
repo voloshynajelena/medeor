6a1c56803912e7d4d74888f21e8d56370e5938e6
Yaroslav Baronin <baronin.yaroslav@gmail.com>
2020-12-13 00:25:49 +0200

    Removed unused variables and methods

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-user-settings',
@@ -7,19 +6,9 @@ import {ThemePalette} from '@angular/material/core';
   styleUrls: ['./user-settings.component.less']
 })
 export class UserSettingsComponent implements OnInit {
-  links = ['First', 'Second', 'Third'];
-  activeLink = this.links[0];
-  background: ThemePalette = undefined;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-  toggleBackground() {
-    this.background = this.background ? undefined : 'primary';
-  }
-
-  addLink() {
-    this.links.push(`Link ${this.links.length + 1}`);
   }
 }
