375da12d91f9a16900ca15854ee10cb8e06016eb
Dmitry Voloshyn <woloshindima@gmail.com>
2021-01-19 21:29:26 +0200

    update snack bar position

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationService {
+    verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+    duration: number = 3000;
+    buttonText: string = 'ok';
 
     constructor(private _snackBar: MatSnackBar) { }
 
     private openSnackBar(message: string, action: string) {
         this._snackBar.open(message, action, {
-            duration: 4000,
+            duration: this.duration,
+            verticalPosition: this.verticalPosition
         });
     }
 
     throwError(message: string) {
-        this.openSnackBar(`Error: ${message}`, 'ok')
+        return this.openSnackBar(`Error: ${message}`, this.buttonText)
     }
 }
