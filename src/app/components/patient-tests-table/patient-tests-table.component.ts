23ac751880c6d1c121bdfd82d686b3cf752d2051
Kateryna <chuprynska.kateryna@gmail.com>
2021-04-02 11:34:55 +0300

    PDFmake first steps

--- src/app/components/patient-tests-table/patient-tests-table.component.ts
+++ src/app/components/patient-tests-table/patient-tests-table.component.ts
@@ -5,7 +5,10 @@ import { PageEvent } from '@angular/material/paginator';
 import { TooltipPosition } from '@angular/material/tooltip';
 import { FormControl } from '@angular/forms';
 
-
+//pdf
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-patient-tests-table',
@@ -293,5 +296,35 @@ export class PatientTestsTableComponent implements OnInit {
     let markedTests = this.tests.filter(t => t.marked);
     //print checked tests to console
     console.log('marked tests to print------>', markedTests);
+  }
+
+  //PDF
+  someContent = 'String to check variable....';
+
+  generatePdf(){
+    
+    const documentDefinition = {
+      content: [
+        // if you don't need styles, you can use a simple string to define a paragraph
+        'This is a standard paragraph, using default style',
+    
+        // using a { text: '...' } object lets you set styling properties
+        { text: 'This paragraph will have a bigger font', fontSize: 15 },
+    
+        // if you set the value of text to an array instead of a string, you'll be able
+        // to style any part individually
+        {
+          text: [
+            'This paragraph is defined as an array of elements to make it possible to ',
+            { text: 'restyle part of it and make it bigger ', fontSize: 15 },
+            'than the rest.'
+          ]
+        },
+
+        'Значение внешней переменной: ' + this.someContent
+      ]
+    };
+    
+    pdfMake.createPdf(documentDefinition).open();
   }
 }
