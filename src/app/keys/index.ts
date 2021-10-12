fe7dd6f4236aa1e2ebefb52e621c6e6f815c417b
Voloshyna Jelena <codeismagicschool@gmail.com>
2021-10-12 19:05:21 +0300

    [fix] email notification

---
+++ src/app/keys/index.ts
@@ -0,0 +1,3 @@
+export default {
+  sendGridApiKey: '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'
+}
---
+++ src/app/utils/emails.ts
@@ -0,0 +1,14 @@
+import { BASE_URL, EMAIL_FROM } from "../constants";
+
+export const regMail = (email: string): any => {
+  return {
+    to: email,
+    from: EMAIL_FROM,
+    subject: 'Account created',
+    html: `
+    <h1>Welcome!</h1>
+    <p>Вы успешно создали аккаунт с email - ${email}</p>
+    <hr/>
+    <a href="${BASE_URL}"></a>`
+  };
+}
