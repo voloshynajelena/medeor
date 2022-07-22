6ac0cfcb4eda84c115bf148e6af32963c8862202
DJUR-DJUR <djurodjur@gmail.com>
2022-07-23 00:22:20 +0300

    unsubscribe onDestroy

--- src/app/components/test-profile/test-profile.component.ts
+++ src/app/components/test-profile/test-profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params} from '@angular/router';
+import { Subscription } from 'rxjs';
 import { TestsService } from 'src/app/services/tests.service';
 import { ITest, Test } from 'src/app/types';
 import { TESTS } from '../clients-table/clients-table.component';
@@ -9,9 +10,11 @@ import { TESTS } from '../clients-table/clients-table.component';
   templateUrl: './test-profile.component.html',
   styleUrls: ['./test-profile.component.scss']
 })
-export class TestProfileComponent implements OnInit {
+export class TestProfileComponent implements OnInit, OnDestroy {
   private testId: string
+  private testIdSubscription: Subscription
   public test: ITest
+  private testSubscription: Subscription
   public loading = false
   
   constructor(
@@ -22,18 +25,24 @@ export class TestProfileComponent implements OnInit {
   ngOnInit(): void {
     this.loading = true;
     
-    this.route.params.subscribe( (params: Params) => {
-      this.testId = params.testid })
+    this.testIdSubscription = this.route.params
+      .subscribe( (params: Params) => {
+        this.testId = params.testid })
 
-    this.testService.getTestsTemplates().subscribe( ({data}) => {
-      this.test = data.find(test => test.typeId === this.testId)
+    this.testSubscription = this.testService
+      .getTestsTemplates()
+      .subscribe( ({data}) => {
+        this.test = data.find(test => test.typeId === this.testId)
+        this.loading = false;
     })
-
-    this.loading = false;
   }
 
   goBack(): void {
     history.back()
   }
 
+  ngOnDestroy(): void {
+   this.testIdSubscription.unsubscribe()
+   this.testSubscription.unsubscribe()
+  }
 }
